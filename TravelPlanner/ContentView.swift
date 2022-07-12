c5d6ac5200b7e18fe8f8177a08cd70bdbe472f98
tanya_bond <bondarenkotatiana96@gmail.com>
2022-07-11 17:50:47 -0700

    Created the project

---
+++ TravelPlanner/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TravelPlanner
+//
+//  Created by Tatiana Bondarenko on 7/11/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TravelPlanner/TravelPlannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TravelPlannerApp.swift
+//  TravelPlanner
+//
+//  Created by Tatiana Bondarenko on 7/11/22.
+//
+
+import SwiftUI
+
+@main
+struct TravelPlannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
