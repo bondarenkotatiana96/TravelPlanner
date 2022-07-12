40ae4bd6e854b582fcfab6a9432ebefdba27fe4e
tanya_bond <bondarenkotatiana96@gmail.com>
2022-07-12 16:26:48 -0700

    Added trip list view with search bar

--- TravelPlanner/Views/TripListView.swift
+++ TravelPlanner/Views/TripListView.swift
@@ -9,14 +9,90 @@ import SwiftUI
 
 struct TripListView: View {
     
+    @State var searchText = ""
+    @State var searching = false
+    
     @ObservedObject var viewModel = TripListViewModel()
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationView {
+            VStack(alignment: .leading) {
+                SearchBar(searchText: $searchText, searching: $searching)
+                
+                List {
+                    ForEach(viewModel.trips) { trip in
+                        NavigationLink {
+                            // TRIP DETAILS VIEW
+                        } label: {
+                            HStack {
+                                Image(systemName: "globe.europe.africa")
+                                Text(trip.name)
+                            }
+                        }
+                    }
+                }
+                .listStyle(.plain)
+                .navigationTitle(searching ? "Searching" : "My Trips")
+                .toolbar {
+                    if searching {
+                        Button("Cancel") {
+                            searchText = ""
+                            withAnimation {
+                                searching = false
+                                UIApplication.shared.dismissKeyboard()
+                            }
+                        }
+                    }
+                }
+                .gesture(DragGesture()
+                    .onChanged({ _ in
+                        UIApplication.shared.dismissKeyboard()
+                    })
+                )
+            }
+        }
     }
 }
 
 struct TripListView_Previews: PreviewProvider {
     static var previews: some View {
         TripListView()
+    }
+}
+
+struct SearchBar: View {
+    @Binding var searchText: String
+    @Binding var searching: Bool
+    
+    var body: some View {
+        ZStack {
+            Rectangle()
+                .fill(.yellow)
+            HStack {
+                Image(systemName: "magnifyingglass")
+                TextField("Search ..", text: $searchText) { startedEditing in
+                    if startedEditing {
+                        withAnimation {
+                            searching = true
+                        }
+                    }
+                } onCommit: {
+                    withAnimation {
+                        searching = false
+                    }
+                }
+            }
+            .foregroundColor(.gray)
+            .padding(.leading, 13)
+        }
+            .frame(height: 40)
+            .cornerRadius(13)
+            .padding()
+    }
+}
+
+extension UIApplication {
+    func dismissKeyboard() {
+        sendAction(#selector(UIResponder.resignFirstResponder), to: nil, from: nil, for: nil)
     }
 }
