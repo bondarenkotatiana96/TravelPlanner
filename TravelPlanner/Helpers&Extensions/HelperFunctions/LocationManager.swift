d7d2eea7740263bc0b57913b75cbd2a15b4c0c7b
tanya_bond <bondarenkotatiana96@gmail.com>
2022-07-21 12:37:27 -0700

    Add location manager and home view layout

---
+++ TravelPlanner/Helpers&Extensions/HelperFunctions/LocationManager.swift
@@ -0,0 +1,31 @@
+//
+//  LocationManager.swift
+//  TravelPlanner
+//
+//  Created by Tatiana Bondarenko on 7/21/22.
+//
+
+import MapKit
+
+class LocationManager: NSObject, ObservableObject {
+    
+    private let locationManager = CLLocationManager()
+    @Published var location: CLLocation? = nil
+    
+    override init() {
+        super.init()
+        self.locationManager.delegate = self
+        self.locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        self.locationManager.requestWhenInUseAuthorization()
+        self.locationManager.startUpdatingLocation()
+    }
+}
+
+extension LocationManager: CLLocationManagerDelegate {
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        guard let location = locations.last else { return }
+        
+        self.location = location
+    }
+}
--- TravelPlanner/Views/HomeView/HomeView.swift
+++ TravelPlanner/Views/HomeView/HomeView.swift
@@ -6,8 +6,11 @@
 //
 
 import SwiftUI
+import MapKit
 
 struct HomeView: View {
+    
+    @ObservedObject private var locationManager = LocationManager()
     
     @State private var hueAdjust = false
     @State var isHidden = true
@@ -15,6 +18,9 @@ struct HomeView: View {
     @State var randomDestination: RandomDestination = RandomDestination(city: "", country: "", latitude: 0.0, longitude: 0.0)
     
     var body: some View {
+        
+        let coordinate = self.locationManager.location != nil ? self.locationManager.location!.coordinate : CLLocationCoordinate2D()
+        NavigationView{
             ZStack {
                 LinearGradient(gradient: Gradient(colors: [Color("AccentText"), Color("AccentPink"), Color("AccentColor")]), startPoint: .bottom, endPoint: .top)
                     .edgesIgnoringSafeArea(.all)
@@ -24,40 +30,35 @@ struct HomeView: View {
                         hueAdjust.toggle()
                     }
                 VStack {
-                    Text("Are you ready for an adventure?")
-                        .font(.title)
-                        .padding(.bottom)
-                        .isHidden(!isHidden, remove: !isHidden)
-                    Text("\(randomDestination.city), \(randomDestination.country)")
-                        .font(.largeTitle)
-                        .padding(.bottom, 30)
-                        .isHidden(isHidden, remove: isHidden)
-                    VStack(alignment: .leading) {
-                        Text("Featured Events")
-                            .italic()
-                            .padding(.leading)
-                        ScrollView(.horizontal) {
-                            HStack(spacing: 20) {
-                                ForEach(0..<10) {
-                                    Text("Item \($0)")
-                                        .foregroundColor(.white)
-                                        .font(.largeTitle)
-                                        .frame(width: 280, height: 200)
-                                        .background(.red)
-                                }
+                    Text("Popular destinations in the USA")
+                    ScrollView(.horizontal) {
+                        HStack(spacing: 20) {
+                            ForEach(0..<10) {
+                                Text("Item \($0)")
+                                    .foregroundColor(.white)
+                                    .font(.largeTitle)
+                                    .frame(width: 250, height: 170)
+                                    .background(Color("BackgroundYellow"))
                             }
                         }
-                        .padding(.bottom)
-                        .padding(.horizontal, 10)
-//                        Button {
-//                            tripListVM.createTrip(name: randomDestination.city, latitude: randomDestination.latitude, longitude: randomDestination.longitude)
-//                        } label: {
-//                            Text("Add to my trip list")
-//                        }
-//                        .padding(.leading)
-//                        .padding(.bottom)
                     }
-                    .isHidden(isHidden, remove: isHidden)
+                    .padding(.bottom)
+                    .padding(.horizontal, 10)
+                    
+                    Text("Local places")
+                    ScrollView(.horizontal) {
+                        HStack(spacing: 20) {
+                            ForEach(0..<10) {
+                                Text("Item \($0)")
+                                    .foregroundColor(.white)
+                                    .font(.largeTitle)
+                                    .frame(width: 250, height: 170)
+                                    .background(Color("BackgroundYellow"))
+                            }
+                        }
+                    }
+                    .padding(.bottom)
+                    .padding(.horizontal, 10)
                     
                     Button {
                         RandomDestinationService().getRandomDestination { result in
@@ -72,9 +73,19 @@ struct HomeView: View {
                         }
                         isHidden.toggle()
                     } label: {
-                        ButtonLabel(text: isHidden ? "Get a random destination" : "Get another destination", imageName: "airplane", width: 320, height: 50, imageSize: 30)
+                        ButtonLabel(text: isHidden ? "Get a random destination" : "Close", imageName: "airplane", width: 320, height: 50, imageSize: 30)
                     }
+                    ZStack {
+                        RoundedRectangle(cornerRadius: 50).fill(Color.accentColor).frame(width: UIScreen.main.bounds.width - 70, height: 50)
+                        Text("\(randomDestination.city), \(randomDestination.country)")
+                            .foregroundColor(Color("AccentText"))
+                            .font(.system(size: 20))
+                    }
+                    .isHidden(isHidden, remove: isHidden)
                 }
+                .navigationTitle("Explore places near you")
+                .navigationBarTitleDisplayMode(.inline)
+            }
         }
     }
 }
@@ -84,3 +95,22 @@ struct HomeView_Previews: PreviewProvider {
         HomeView()
     }
 }
+
+//    VStack(alignment: .leading) {
+//        Text("Featured Events")
+//            .italic()
+//            .padding(.leading)
+//        ScrollView(.horizontal) {
+//            HStack(spacing: 20) {
+//                ForEach(0..<10) {
+//                    Text("Item \($0)")
+//                        .foregroundColor(.white)
+//                        .font(.largeTitle)
+//                        .frame(width: 280, height: 200)
+//                        .background(.red)
+//                }
+//            }
+//        }
+//        .padding(.bottom)
+//        .padding(.horizontal, 10)
+//    }
